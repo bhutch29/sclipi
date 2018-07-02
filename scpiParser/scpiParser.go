41af4bda6ba4467f4b51e43f4120a30a6a6499b2
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2018-07-02 10:37:00 -0700

    Completed work for recursive suffix solving, but there is an issue with splitting on all colons, since colons are part of the suffix syntax

--- scpiParser/scpiParser.go
+++ scpiParser/scpiParser.go
@@ -1,42 +1,106 @@
 package scpiParser
 
-import "strings"
+import (
+	"strings"
+	"regexp"
+	"strconv"
+	"fmt"
+)
 
 func splitScpiCommands(lines []string) [][]string {
 	var commands [][]string
 	for _, line := range lines {
-		line = strings.Replace(line,"[", "", -1)
+		line = strings.Replace(line, "[", "", -1)
 		trimmed := strings.TrimLeft(line, ":")
 		split := strings.Split(trimmed, ":")
+		withOptionals := generateOptionalCommands(split, getOptionalIndexes(split))
 
-		var includingOptionals [][]string
-		includingOptionals = append(includingOptionals, removeSquareBraces(split)) //Add case where everything is used
-
-		optionals := getOptionals(split)
-		for _, index := range optionals{ //TODO: Only handles case with single optional, need to recurse
-			var a []string
-			a = append(split[:index], split[index+1:]...) //delete element of optional index from slice
-			includingOptionals = append(includingOptionals, removeSquareBraces(a))
+		var withSuffixes [][]string
+		for _, command := range withOptionals {
+			withSuffixes = append(withSuffixes, generateSuffixedCommands(command)...)
 		}
 
-		//TODO: add generated list to commands variable
+		for _, command := range withSuffixes {
+			commands = append(commands, command)
+		}
+	}
+	return commands
+}
+func generateSuffixedCommands(command []string) [][]string {
+	var commands [][]string
+	suffixed := false
+	for i, word := range command{
+		if isSuffixed(word) {
+			suffixed = true
+			for _, suffix := range branchSuffixes(word){
+				temp := command
+				temp[i] = suffix
+				generateSuffixedCommands(temp)
+			}
+		}
+	}
+
+	if suffixed {
+		return [][]string{}
+	} else {
+		return commands
+	}
+
+}
+func branchSuffixes(s string) []string{
+	var suffixes []string
+	r, _ := regexp.Compile("([^{]*){([0-9]):([0-9])}")
+	result := r.FindStringSubmatch(s)
+	prefix := result[1]
+	start, err := strconv.Atoi(result[2])
+	if err != nil {
+		fmt.Println("Error parsing start value of suffix, e.g. {1:2}. Error = " + err.Error())
+		return []string{}
+	}
+	stop, err := strconv.Atoi(result[3])
+	if err != nil {
+		fmt.Println("Error parsing stop value of suffix, e.g. {1:2}. Error = " + err.Error())
+		return []string{}
+	}
+
+	for i := start; i <= stop; i++  {
+		suffixes = append(suffixes, prefix + strconv.Itoa(i))
+	}
+
+	return suffixes
+}
+
+func isSuffixed(s string) bool{
+	return strings.Contains(s, "{")
+}
+
+func generateOptionalCommands(command []string, optionalIndexes []int) [][]string {
+	commands := [][]string{removeSquareBraces(command)}
+	for i, index := range optionalIndexes {
+		shortened := deleteStringFromSlice(command, index) //TODO: if last element is optional, deletes query information
+		remaining := optionalIndexes[i+1:]
+		commands = append(commands, generateOptionalCommands(shortened, remaining)...)
 	}
 	return commands
 }
 
-func getOptionals(words []string) []int{
+func deleteStringFromSlice(split []string, index int) []string {
+	return append(split[:index], split[index+1:]...)
+}
+
+func getOptionalIndexes(words []string) []int {
 	var optionals []int
-	for i, item := range words{
-		if strings.Contains(item,"]") {
+	for i, item := range words {
+		if strings.Contains(item, "]") {
 			optionals = append(optionals, i)
 		}
 	}
 	return optionals
 }
 
-func removeSquareBraces(words []string) []string{
+func removeSquareBraces(words []string) []string {
 	var result []string
-	for _, word := range words{
+	for _, word := range words {
 		result = append(result, strings.Replace(word, "]", "", -1))
 	}
 	return result
--- scpiParser/scpiParser_test.go
+++ scpiParser/scpiParser_test.go
@@ -7,8 +7,18 @@ import (
 )
 
 func TestScpiParser(t *testing.T) {
-	lines := []string {":ABORt/nquery/"}
+	lines := []string {":Example{1:2}:Afterward"}
 	commands := splitScpiCommands(lines)
+	if len(commands) != 4 {
+		t.Error(":Example{1:2}:Afterward not parsed properly:", commands[0])
+		return
+	}
+	if len(commands[0]) != 2 {
+		t.Error(":Example{1:2}:Afterward not parsed properly:", commands[0])
+	}
+
+	lines = []string {":ABORt/nquery/"}
+	commands = splitScpiCommands(lines)
 	if len(commands) != 1 {
 		t.Error(":ABORt/nquery/ not parsed properly:", commands[0])
 		return
@@ -23,10 +33,10 @@ func TestScpiParser(t *testing.T) {
 		t.Error(":ABORt[:SWEep]/nquery/ not parsed properly:", commands)
 		return
 	}
-	if len(commands[0]) != 1 {
+	if len(commands[0]) != 2  {
 		t.Error(":ABORt[:SWEep]/nquery/ not parsed properly:", commands[0])
 	}
-	if len(commands[1]) != 2 {
+	if len(commands[1]) != 1 {
 		t.Error(":ABORt[:SWEep]/nquery/ not parsed properly:", commands[1])
 	}
 
@@ -41,6 +51,21 @@ func TestScpiParser(t *testing.T) {
 	}
 	if len(commands[1]) != 4 {
 		t.Error(":CALibration:BBG:CHANnel:OFFSet not parsed properly:", commands[1])
+	}
+}
+
+func TestBranchSuffixes(t *testing.T) {
+	result := branchSuffixes("Example{1:1}")
+	if len(result) != 1 {
+		t.Error("Example{1:1} not parsed to 1 result", result)
+	}
+
+	result = branchSuffixes("Example{1:3}")
+	if len(result) != 3 {
+		t.Error("Example{1:3} not parsed to 3 results", result)
+	}
+	if result[0] != "Example1"{
+		t.Error("Example{1:3} first element not Example1", result)
 	}
 }
 

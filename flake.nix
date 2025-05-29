730b95b7166a8def9a19fe8d34cc4a2133fa6037
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2025-05-29 10:33:08 -0700

    added flake.nix

---
+++ flake.nix
@@ -0,0 +1,52 @@
+{
+  description = "SCPI CLI";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        packages = {
+          default = pkgs.buildGoModule {
+            pname = "sclipi";
+            version = "0.4.0";
+            src = ./.;
+            vendorHash = null; # Set to actual hash after first build
+            meta = {
+              description = "SCPI CLI";
+            };
+          };
+
+          nixosModule = { config, lib, pkgs, ... }:
+            let
+              cfg = config.programs.sclipi;
+              sclipiPkg = self.packages.${system}.default;
+            in
+            {
+              options.programs.sclipi = {
+                enable = lib.mkEnableOption "sclipi";
+              };
+              config = lib.mkIf cfg.enabled {
+                environment.systemPackages = [ sclipiPkg ];
+              };
+            };
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            go
+            gopls
+            gotools
+          ];
+        };
+      };
+    );
+}

63402ca9a94afd7ae82d25c1d2acc28a796b13dc
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2025-10-19 09:50:54 -0700

    remove unused files

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+// import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 
-import { routes } from './app.routes';
+// import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    // provideRouter(routes),
     provideHttpClient()
   ]
 };

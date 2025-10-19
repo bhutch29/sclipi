8f1c8756bb28b611fee24cc469000c6b219739c8
Bradley Hutchinson <bradley.hutchinson@keysight.com>
2025-10-19 10:24:51 -0700

    Added Preferences Component

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -19,6 +19,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatButtonModule } from '@angular/material/button';
 import { LocalStorageService } from '../services/localStorage.service';
 import { PreferencesService } from '../services/preferences.service';
+import { PreferencesComponent } from './preferences/preferences.component';
 
 interface LogEntry {
   type: 'command' | 'query';
@@ -39,7 +40,7 @@ interface ScpiResponse {
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [FormsModule, DatePipe, CommonModule, MatAutocompleteModule, MatInputModule, MatFormFieldModule, MatButtonModule],
+  imports: [FormsModule, DatePipe, CommonModule, MatAutocompleteModule, MatInputModule, MatFormFieldModule, MatButtonModule, PreferencesComponent],
 })
 export class App {
   public inputText = signal('');
@@ -185,62 +186,6 @@ export class App {
 
   public systErr() {
     this.sendInternal(':SYST:ERR?');
-  }
-
-  public onPortBlur() {
-    this.setPort(this.preferences.port());
-  }
-
-  public onPortEnter(event: Event) {
-    event.preventDefault();
-    this.setPort(this.preferences.port());
-  }
-
-  private setPort(port: number) {
-    if (!Number.isInteger(port)) {
-      console.error('port must be an integer', port);
-      this.preferences.port.set(this.preferences.committedPort());
-      return;
-    }
-
-    if (port < 1 || port > 65535) {
-      console.error('port must be between 1 and 65535', port);
-      this.preferences.port.set(this.preferences.committedPort());
-      return;
-    }
-
-    if (this.preferences.committedPort() != this.preferences.port()) {
-      this.preferences.committedPort.set(this.preferences.port());
-      if (port !== 0) {
-        this.http.post('/api/scpiPort', this.preferences.committedPort(), { responseType: 'text' }).subscribe({
-          next: (x) => console.log(x),
-          error: (x) => console.error('Error posting port value', this.preferences.committedPort(), x),
-        });
-      }
-    }
-  }
-
-  public onAddressBlur() {
-    this.setAddress(this.preferences.address());
-  }
-
-  public onAddressEnter(event: Event) {
-    event.preventDefault();
-    this.setAddress(this.preferences.address());
-  }
-
-  private setAddress(address: string) {
-    if (this.preferences.committedAddress() != this.preferences.address()) {
-      this.preferences.committedAddress.set(this.preferences.address());
-      if (address !== '') {
-        this.http
-          .post('/api/scpiAddress', this.preferences.committedAddress(), { responseType: 'text' })
-          .subscribe({
-            next: (x) => console.log(x),
-            error: (x) => console.error('Error posting address value', this.preferences.committedAddress(), x),
-          });
-      }
-    }
   }
 
   public clearHistory() {
---
+++ web/src/app/preferences/preferences.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+import { PreferencesService } from '../../services/preferences.service';
+import { HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-preferences',
+  imports: [FormsModule],
+  templateUrl: './preferences.component.html',
+  styleUrl: './preferences.component.scss'
+})
+export class PreferencesComponent {
+  constructor(
+    public preferences: PreferencesService,
+    private http: HttpClient
+  ) {
+  }
+
+  public onPortBlur() {
+    this.setPort(this.preferences.port());
+  }
+
+  public onPortEnter(event: Event) {
+    event.preventDefault();
+    this.setPort(this.preferences.port());
+  }
+
+  private setPort(port: number) {
+    if (!Number.isInteger(port)) {
+      console.error('port must be an integer', port);
+      this.preferences.port.set(this.preferences.committedPort());
+      return;
+    }
+
+    if (port < 1 || port > 65535) {
+      console.error('port must be between 1 and 65535', port);
+      this.preferences.port.set(this.preferences.committedPort());
+      return;
+    }
+
+    if (this.preferences.committedPort() != this.preferences.port()) {
+      this.preferences.committedPort.set(this.preferences.port());
+      if (port !== 0) {
+        this.http.post('/api/scpiPort', this.preferences.committedPort(), { responseType: 'text' }).subscribe({
+          next: (x) => console.log(x),
+          error: (x) => console.error('Error posting port value', this.preferences.committedPort(), x),
+        });
+      }
+    }
+  }
+
+  public onAddressBlur() {
+    this.setAddress(this.preferences.address());
+  }
+
+  public onAddressEnter(event: Event) {
+    event.preventDefault();
+    this.setAddress(this.preferences.address());
+  }
+
+  private setAddress(address: string) {
+    if (this.preferences.committedAddress() != this.preferences.address()) {
+      this.preferences.committedAddress.set(this.preferences.address());
+      if (address !== '') {
+        this.http
+          .post('/api/scpiAddress', this.preferences.committedAddress(), { responseType: 'text' })
+          .subscribe({
+            next: (x) => console.log(x),
+            error: (x) => console.error('Error posting address value', this.preferences.committedAddress(), x),
+          });
+      }
+    }
+  }
+}
